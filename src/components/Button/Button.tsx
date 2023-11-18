439c00e2d97e61d23e23f438367c582e914746ef
aramoh3ni <alireza.mohseni.se@gmail.com>
2023-11-18 16:13:16 +0430

    init startup point

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>
+
+const Button: React.FC<ButtonProps> = () => <div>Button</div>
+
+export default Button
